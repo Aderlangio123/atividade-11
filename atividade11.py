8d1510f93a0d8735ddbe19ecf381e7d50db01200
Aderlangio123 <faderlangio123@gmail.com>
2024-09-11 14:56:56 -0300

    atividade 11 - verificar idade

---
+++ atividade11.py
@@ -0,0 +1,23 @@
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+# Criança (0-12 anos)
+# Adolescente (13-17 anos)
+# Adulto (18-59 anos)
+# Idoso (60 anos ou mais)
+
+
+idade = int(input("digite sua idade! :"))
+
+
+if idade <= 12:
+    print("voce é uma criança! ")
+
+if idade >= 13 and idade <= 17 :
+    print("voce é um adolescente!  ")
+
+if idade >= 18 and idade <= 59 :
+    print("voce é um adulto!  ")
+
+if idade >= 60 :
+    print("voce é um idoso!  ")
+    
+
