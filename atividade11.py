3b41537d787535edc09b61dd22f78446d6202c5d
Aderlangio123 <faderlangio123@gmail.com>
2024-09-13 11:35:46 -0300

    atividade 11 - idade

--- atividade11.py
+++ atividade11.py
@@ -11,13 +11,14 @@ idade = int(input("digite sua idade! :"))
 if idade <= 12:
     print("voce é uma criança! ")
 
-if idade >= 13 and idade <= 17 :
+elif idade >= 13 and idade <= 17 :
     print("voce é um adolescente!  ")
 
-if idade >= 18 and idade <= 59 :
+elif  idade >= 18 and idade <= 59 :
     print("voce é um adulto!  ")
 
-if idade >= 60 :
+elif idade >= 60 :
     print("voce é um idoso!  ")
+    
     
 
